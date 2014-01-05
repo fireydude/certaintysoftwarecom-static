a5955fe694cc0d30f5b852e64501189decaf130e
Brian Herbert <briandangerous@gmail.com>
2014-01-05 17:14:17 +0000

    store work experience in js Objects

---
+++ script/contracts.js
@@ -0,0 +1,17 @@
+﻿
+function WorkExperience(title, dates, desc) {
+    this.title = title;
+    this.dates = dates;
+    this.desc = desc;
+}
+
+var contracts = [new WorkExperience("Matrix Telematics",
+    "Feb 2013 - Mar 2013",
+    "Amazon Web Services"),
+    new WorkExperience("Manheim Retail Services",
+        "Jan 12 - July 12",
+        "Enterprise solutions for B2B websites")];
+
+for(var i = 0; i<contracts.len; i++) {
+    alert(contracts[i].title);
+}
---
+++ script/workExperience.js
@@ -0,0 +1,30 @@
+﻿
+function addExperienceAccordion(id, title, works) {
+    var accordionGroup = '<h2>' + title + '</h2><div class="accordion-group">';
+    for (var i = 0; i < works.length; i++) {
+        accordionGroup += '<div class="accordion-heading">';
+        accordionGroup += '<a class="accordion-toggle btn-lft btn" data-toggle="collapse" data-parent="#' + id + '-accordion"';
+        accordionGroup += 'href="#' + works[i].id + '">' + works[i].title + '</a>';
+        accordionGroup += '</div>';
+        accordionGroup += '<div id="' + works[i].id + '" class="acccordion-body collapse">';
+        accordionGroup += '<div class="accordion-inner">';
+        accordionGroup += '<h5>' + works[i].dates + '</h5>';
+        accordionGroup += '<p>' + works[i].desc + '</p>';
+        if (typeof(works[i].sub) != 'undefined') {
+            accordionGroup += '<h5>' + works[i].sub + '</h5>';
+            accordionGroup += '<p>' + works[i].subText + '</p>';
+        }
+        accordionGroup += '</div>';
+        accordionGroup += '</div>';
+    }
+    accordionGroup += '</div>';
+    $('#' + id + '-accordion').append(accordionGroup);
+}
+function WorkExperience(id, title, dates, desc, sub, subText) {
+    this.id = id;
+    this.title = title;
+    this.dates = dates;
+    this.desc = desc;
+    this.sub = sub;
+    this.subText = subText;
+}
