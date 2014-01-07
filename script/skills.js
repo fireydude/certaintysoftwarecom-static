5038c3e0b79aed34f8b2148973a12b15723a53f8
Brian Herbert <briandangerous@gmail.com>
2014-01-07 12:43:56 +0000

    jit hypertree

---
+++ script/skills.js
@@ -0,0 +1,40 @@
+﻿
+var json =
+    {
+        "id": "1",
+        "name": "Technology",
+        "children": [{
+            "id": "2",
+            "name": "Web",
+            "children": [{
+                "id": "2a",
+                "name": "HTML5"
+            }, {
+                "id": "2b",
+                "name": "CSS"
+            }, {
+                "id": "2c",
+                "name": "JavaScript",
+                "children": [{ "id": "2c1", "name": "jQuery" }, { "id": "2c2", "name": "jQuery UI" }, { "id": "2c3", "name": "AngularJS" }, { "id": "2c4", "name": "InfoVis Toolkit" }]
+            }
+            ]
+        },
+            {
+                "id": "3",
+                "name": ".NET",
+                "children": [
+                    { "id": "3a", "name": "C# 4.5" },
+                    {
+                        "id": "3b", "name": "ASP .NET",
+                        "children": [{ "id": "3b1", "name": "MVC 4" }]
+                    },
+                    { "id": "3c", "name": "Entity Framework" },
+                    { "id": "3D", "name": "Castle Active Record (NHibernate)" }]
+            },
+            {
+                "id": "4",
+                "name": "Database",
+                "children": [{ "id": "4a", "name": "SQL Server" }, { "id": "4b", "name": "MySQL" }]
+            }],
+        "data": []
+    };
