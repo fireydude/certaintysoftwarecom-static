cc65c82fcb67778580c7635a2dba725b682bc76b
Brian Herbert <briandangerous@gmail.com>
2016-06-12 15:26:29 +0100

    WorkExperienceGroup model

--- script/workExperience.js
+++ script/workExperience.js
@@ -9,13 +9,22 @@ function WorkExperience(id, header, dates, desc, sub, subText) {
     this.subText = subText;
 }
 
+// model
+function WorkExperienceGroup(id, header, items, desc) {
+    this.id = id;
+    this.header = header;
+    this.items = items;
+    this.desc = desc;
+}
+
 // controller
 var workExperienceCtrl = function(){
-    this.contractExperience = new WorkExperience("contract", "Contract", contracts, "Worked for these companies");
-    this.freelanceExperience = new WorkExperience("freelance", "Freelance", freelance, "Projects undertaken for these companies");
-    this.permExperience = new WorkExperience("perm", "Permanent", perms, "Employed by these companies");
-    console.log(this);
+    this.contractExperience = new WorkExperienceGroup("contract", "Contract", contracts, "Worked for these companies");
+    this.freelanceExperience = new WorkExperienceGroup("freelance", "Freelance", freelance, "Projects undertaken for these companies");
+    this.permExperience = new WorkExperienceGroup("perm", "Permanent", perms, "Employed by these companies");
+
 };
+
 angular.module('cs')
     .controller('workExperienceCtrl', workExperienceCtrl);
 
@@ -24,8 +33,7 @@ angular.module('cs')
         return {
             restrict: 'E',
             scope: {
-                header: '=type',
-                items: '=works'
+                model: '=type',
             },
             templateUrl: "experienceAccordion.html"
         };
